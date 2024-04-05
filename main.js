d72d33ac0ea7a6783f8e594256ef53e094a52361
342 Video editing <342-cab@thpc.dc>
2024-04-05 12:48:36 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function calculateIMT () {
+    var weight = document.getElementById('weight').value
+    var height = document.getElementById('height').value
+    console.log("вес:" , weight)
+
+    let imt = weight / (height ** 2)
+}
+
+
+
+    function calculateIMT () {
+    amtText += "недостатычно (дефицыт) масса тела "
+    if 
+}
+
